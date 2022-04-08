75cbbda1ec7023baac1090a9ab28e1fcc85cae70
cgcouto <cgcouto1337@gmail.com>
2022-04-07 21:04:41 -0700

    Worked on tap line, getting lists home screen up and running

--- src/HomeView.js
+++ src/HomeView.js
@@ -3,7 +3,8 @@ import ListBox from "./ListBox";
 import {initializeApp} from "firebase/app";
 import { getFirestore, collection, query, setDoc, doc, updateDoc, deleteDoc, serverTimestamp, orderBy} from "firebase/firestore";
 import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
+
 import {useCollectionData} from "react-firebase-hooks/firestore";
 
 
@@ -23,13 +24,24 @@ import {useCollectionData} from "react-firebase-hooks/firestore";
 
 
 function HomeView(props) {
+    const [showName, setShowName] = useState(false)
+    const [name, setName] = useState("");
+
+    const [showDelete, setShowDelete] = useState(false)
+    const [changeThis, setChangeThis] = useState("")
+    const [showRename, setShowRename] = useState(false)
+    const [rename, setRename] = useState("")
+
     let [lists,loading,error] = useCollectionData(collection(props.db, props.collection));
     if (error) {
         console.log("ERROR: List data failed to load from Firestore")
     }
 
     function handleListAdded(listName) {
+        console.log("add")
         let listId = generateUniqueID();
+        setName("")
+        setShowName(false)
         setDoc(doc(props.db, props.collection, listId),
             {
                 key: listId,
@@ -37,8 +49,32 @@ function HomeView(props) {
             }).then(() => {})
     }
 
+
     function handleListDeleted(listId) {
+        console.log("delete")
+        setChangeThis("");
+        setShowDelete(!showDelete)
         deleteDoc(doc(props.db, props.collection, listId)).then(() => {});
+    }
+
+    function handleListRenamed(listId, newName) {
+        setRename("")
+        setChangeThis("")
+        setShowRename(!showRename)
+        updateDoc(doc(props.db, props.collection, listId),
+            {
+                name: newName,
+            }).then(() => {})
+    }
+
+    function handleDeleteToggle(listId) {
+        setChangeThis(listId);
+        setShowDelete(!showDelete);
+    }
+
+    function handleRenameToggle(listId) {
+        setChangeThis(listId)
+        setShowRename(!showRename)
     }
 
     if (loading) {
@@ -46,7 +82,6 @@ function HomeView(props) {
         </div>;
     }
 
-    console.log("TEST")
     return (<div id="contained">
                 <div id="titled">
                     <h1 className={"header"}> Notes </h1>
@@ -56,18 +91,66 @@ function HomeView(props) {
                     {lists?.map((data) => <ListBox id = {data.key}
                                                    name = {data.name}
                                                    onListView = {props.onListView}
-                                                   onListDelete = {handleListDeleted}
+                                                   onDeleteToggle = {handleDeleteToggle}
+                                                   onRenameToggle = {handleRenameToggle}
                     />)}
                 </div>
 
                 <div>
-                    <button id="addnotebutton" onClick={(e) => handleListAdded(generateUniqueID())}><div>+</div></button>
+                    <button id="addnotebutton" onClick={(e) => setShowName(!showName)}><div>+</div></button>
                     <button id="settings">
                         <div>
                             <img src="https://icon-library.com/images/white-gear-icon-png/white-gear-icon-png-7.jpg" width="30" height="30"/>
                         </div>
                     </button>
                 </div>
+        {showDelete && <div>
+            <div>
+                <div id={"back"} onClick={() => setShowDelete(false)}/>
+                <div id={"warning"}>
+                    <div>
+                        This list will be <span id={"deleteText"}>permanently deleted</span>.
+                        Are you sure you want to do this?
+                    </div>
+                    <div id={"warningButtons"}>
+
+                        <div id={"no"} onClick={(e) => setShowDelete(false)}>No, Go Back</div>
+                        <div id={"yes"} onClick={(e) => handleListDeleted(changeThis)}>Yes, Delete</div>
+
+                    </div>
+                </div>
+            </div>
+        </div>}
+        {showName && <div>
+            <div>
+                <div id={"back"} onClick={() => setShowName(false)}/>
+                <div id={"warning"}>
+                    <div id={"nameMessage"}> Name your note and press 'Enter' to confirm.</div>
+                    <input type={"text"}
+                           onChange ={(e) => {setName(e.target.value)}}
+                           onKeyDown={(e) => {if (e.key === 'Enter') {
+                                handleListAdded(name)
+                                }
+                           }}></input>
+
+                    </div>
+                </div>
+            </div>}
+        {showRename && <div>
+            <div>
+                <div id={"back"} onClick={() => setShowRename(false)}/>
+                <div id={"warning"}>
+                    <div id={"nameMessage"}> Rename the selected note and press 'Enter' to confirm.</div>
+                    <input type={"text"}
+                           onChange ={(e) => {setRename(e.target.value)}}
+                           onKeyDown={(e) => {if (e.key === 'Enter') {
+                               handleListRenamed(changeThis, rename)
+                           }
+                           }}></input>
+
+                </div>
+            </div>
+        </div>}
             </div>)
 }
 
--- src/ListBox.js
+++ src/ListBox.js
@@ -1,15 +1,25 @@
 import './ListBox.css'
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 
 
 function ListBox(props) {
 
+    const [settings, setSettings] = useState(false)
+
     return (
         <div>
-            <button className="settings" onClick={(e) => props.onListDelete(props.id)}></button>
-            <div className="notes"  onClick={(e) => props.onListView(props.name, props.id)}>
+
+        {/*  fix tabbing order here  */}
+        <div className="notes">
+            <div className={"listbox"} onClick={(e) => props.onListView(props.name, props.id)}>
                 {props.name}
             </div>
+            <button className="settings" onClick={(e) => setSettings(!settings)}></button>
+        </div>
+            {settings && <div>
+                <div className={"options"} onClick={(e) => props.onDeleteToggle(props.id)}>Delete</div>
+                <div className={"options"} onClick={(e) => props.onRenameToggle(props.id)}>Rename</div>
+            </div>}
         </div>
     )
 }
